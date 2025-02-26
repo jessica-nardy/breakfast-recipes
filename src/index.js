8147dd963dce5dc2bfbf1535e008511779f9b7a5
jessica-nardy <nardy.arq@gmail.com>
2025-02-26 20:46:56 +0000

    Improved the User Experience - UX

--- src/index.js
+++ src/index.js
@@ -13,9 +13,13 @@ function handleSubmit(event) {
   let userInput = document.querySelector("#user-input");
   let apiKey = "8t7b43fadd5ba0704f357ba3a35e30o0";
   let context =
-    "You are a worldwide famous michelin chef and specialized in breakfasts. Your mission is to generate just ONE very consice and simple recipe in basic HTML. Do NOT display the word HTML in the beginning. Make sure to follow the user insctructions.";
+    "You are a worldwide famous michelin chef, specialized in breakfasts which is also talented with words. Your mission is to generate just ONE very consice and simple recipe in basic HTML and written in a fun, but professional tone. Do NOT display the word HTML in the beginning. Also do NOT display ANY sentence in the beginning and at the end, just the recipe, please. Make sure to follow the user insctructions.";
   let prompt = `User instructions: Generate a breakfast recipe with ${userInput.value}`;
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  let recipeElement = document.querySelector("#recipe");
+  recipeElement.classList.remove("hidden");
+  recipeElement.innerHTML = `<div class="blink"> 💡 Generating recipe with ${userInput.value}</div>`;
 
   axios.get(apiUrl).then(displayRecipe);
 }
